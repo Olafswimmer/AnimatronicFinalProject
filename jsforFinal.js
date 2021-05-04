56ff67083145f681e592df31000337095c35cd98
Olafswimmer <stevense@guilford.edu>
2021-05-04 02:14:42 -0400

    finished a lot of stuff

--- jsforFinal.js
+++ jsforFinal.js
@@ -1,1 +1,10 @@
-//hi :)
+increasebutton = document.getElementById("increasebutton");  
+increasestuff = document.getElementById("fonttoincrease");
+everestincrease = document.getElementById("everestfloat");
+joeincrease = document.getElementById("rohdequote");
+increasebutton.addEventListener("click", onButtonClick);
+function onButtonClick() {    
+    increasestuff.style.fontSize = "1.5em";
+    everestincrease.style.width = "195px";
+    joeincrease.style.width = "225px";
+}
