5f41fc6e35a5645fd2f604351c6930270c6cba6f
Olafswimmer <stevense@guilford.edu>
2021-05-05 15:35:12 -0400

    added buttons

--- jsforFinal.js
+++ jsforFinal.js
@@ -3,15 +3,15 @@ increasestuff = document.getElementById("fonttoincrease");
 everestincrease = document.getElementById("everestfloat");
 joeincrease = document.getElementById("rohdequote");
 increasebutton.addEventListener("click", onButtonClick);
+decreasebutton = document.getElementById("decreasebutton");  
+decreasebutton.addEventListener("click", onButtonClick2);
 function onButtonClick() {    
     increasestuff.style.fontSize = "1.5em";
     everestincrease.style.width = "195px";
     joeincrease.style.width = "225px";
 }
-decreasebutton = document.getElementById("decreasebutton");  
-decreasebutton.addEventListener("click", onButtonClick2);
 function onButtonClick2() {    
     increasestuff.style.fontSize = "";
-    everestincrease.style.width = "140px";
-    joeincrease.style.width = "200px";
+    everestincrease.style.width = "";
+    joeincrease.style.width = "";
 }
